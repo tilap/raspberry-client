3b55f124c88c41a5d6332f33d8a4eb58169d171c
Christophe Hurpeau <christophe@hurpeau.com>
2016-02-25 11:06:18 +0100

    stop display on exit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import { stop as stopDisplay } from './display';
 import { close as clientClose } from './tcp-client';
 import { close as serverClose } from './tcp-server';
 import './display';
 
 process.on('SIGINT', () => {
     Promise.all([
+        stopDisplay(),
         clientClose(),
         serverClose(),
     ]).then(() => {
